bb6fe4bf3fce8b67d63eb85fea5ae8f49a2498eb
Yasemin CUNİ <yasemin.cuni@TRHQMLCHQTKY.local>
2023-02-05 18:23:02 +0300

    token integration

--- src/server.js
+++ src/server.js
@@ -1,42 +1,82 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import { db, connectToDb} from './db.js'
+import fs from 'fs';
+import admin, { auth } from 'firebase-admin'
+
+const credentials = JSON.parse(
+  fs.readFileSync('../credentials.json')
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+})
 
 const app = express();
 app.use(express.json());
 
+app.use( async (req, res, next) => {
+  const { authtoken } = req.headers;
+  if (authtoken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authtoken)
+    } catch (e) {
+      res.sendStatus(400);
+    }
+  }
+  next();
+});
+
 app.get('/api/articles/:name', async (req,res)=>{
   const {name} = req.params;
-
+  const {uid} = req.user;
   const article = await db.collection('articles').findOne({name});
 
   if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.include(uid)
     res.json(article); 
   } else {
     res.sendStatus(404)
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
   }
 })
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
   const {name} = req.params;
 
-  await db.collection('articles').updateOne({name}, {
-    $inc: {upvote : 1}
-  })
-
+  const {uid} = req.user;
   const article = await db.collection('articles').findOne({name});
 
   if (article) {
-    res.send(article);
+    const upvoteIds = article.upvoteIds || [];
+    const canUpvote = uid && !upvoteIds.include(uid)
+    if (canUpvote) {
+      await db.collection('articles').updateOne({name}, {
+        $inc: {upvote : 1},
+        $push: {upvoteIds:uid}
+      })  
+    }
+  const updatedArticle = await db.collection('articles').findOne({name});
+
+    res.json(updatedArticle); 
   } else {
-    res.send(`That article doesn\'t exist.`);
+    res.sendStatus(404)
   }
+
 })
 
 app.post('/api/articles/:name/comments', async (req, res) => {
   const {name} = req.params;
-  const {postBy, text} = req.body;
-
+  const {text} = req.body;
+  const {email} = req.user;
   await db.collection('articles').updateOne({name}, {
     $push: {comments: {postBy, text}}
   })
